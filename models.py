fd9e460e4f3676321530f2104b081f28cf636634
Bill Leddy <bill@williesworkshop.net>
2018-09-01 16:23:24 -0700

    News is basically working now

--- models.py
+++ models.py
@@ -6,15 +6,14 @@ class Article(SqliteTable):
     def __init__(self,db_connection):
         super().__init__(db_connection)
         self.table_name = 'article'
-        self.order_by_col = 'publictation_date'
-        self.defaults = {'status': 'draft'}
+        self.order_by_col = 'publication_date desc '
+        self.defaults = {}
         
     def create_table(self):
         """Define and create the article table"""
         
         sql = """
-            status TEXT DEFAULT 'draft',
-            publictation_date DATETIME,
+            publication_date DATETIME,
             title TEXT NOT NULL,
             slug TEXT NOT NULL UNIQUE,
             words TEXT
--- views/news.py
+++ views/news.py
@@ -1,45 +1,133 @@
 from flask import request, session, g, redirect, url_for, abort, \
      render_template, flash, Blueprint, Response
 from users.admin import login_required, table_access_required
+from users.utils import cleanRecordID, printException, render_markdown_for
+from news.models import Article
 from datetime import datetime, timedelta
-import calendar
-import mistune # for Markdown rendering
-import os
 
-mod = Blueprint('www',__name__, template_folder='../templates', url_prefix='/news')
+mod = Blueprint('news',__name__, template_folder='../templates', url_prefix='/news')
 
 
 def setExits():
     g.homeURL = url_for('.display')
+    g.editURL = url_for('.edit')
+    g.listURL = url_for('.display')
+    g.deleteURL = url_for('.delete')
     g.title = 'News'
 
-@mod.route('/')
+@mod.route('/', methods=["GET",])
 def display():
     setExits()
-
-    rendered_html = render_markdown_for(mod,'index.md')
+    #import pdb; pdb.set_trace()
+    rendered_html = render_markdown_for(__file__,mod,'news/news.md')
     
+    recs = Article(g.db).select()
     
-    return render_template('news.html',
-        rendered_html=rendered_html, 
+    return render_template('news/news.html',
+        rendered_html=rendered_html, recs = recs,
         )    
 
-
-def render_markdown_for(mod,file_name):
-    """Try to find the file to render and then do so"""
-    rendered_html = ''
-    # use similar search approach as flask templeting, root first, then local
-    # try to find the root templates directory
-    markdown_path = os.path.dirname(os.path.abspath(__name__)) + '/templates/{}'.format(file_name)
-    if not os.path.isfile(markdown_path):
-        # look in the templates directory of the calling blueprint
-        markdown_path = os.path.dirname(os.path.abspath(__file__)) + '/{}/{}'.format(mod.template_folder,file_name)
-    if os.path.isfile(markdown_path):
-        f = open(markdown_path)
-        rendered_html = f.read()
-        f.close()
-        rendered_html = mistune.markdown(rendered_html)
+@mod.route('/edit', methods=["GET","POST"])
+@mod.route('/edit/', methods=["GET","POST"])
+@mod.route('/edit/<article_handle>', methods=["GET","POST"])
+@mod.route('/edit/<article_handle>/', methods=["GET","POST"])
+@table_access_required(Article)
+def edit(article_handle='0'):
+    setExits()
+    g.title = "Article"
+    articles = Article(g.db)
+    rec_id = None
     
-    return rendered_html
+    #Is the handle an existing slug?
+    if len(article_handle) > 0:
+        # The slug is arbitrary text, so be careful!!!
+        sql = 'select * from article where slug = ? order by id'
+        rec = articles.select_one_raw(sql,(article_handle,))
+    if not rec:
+        rec_id = cleanRecordID(article_handle)
+        if rec_id < 0:
+            #is the handle < o? ... bad request, go home
+            flash('That is not a valid article slug')
+            return redirect(g.homeURL)
+            
+        if rec_id > 0:
+            rec = articles.get(rec_id)
+            if not rec:
+                flash('That is not a valid article id')
+                return redirect(g.homeURL)
+        else:
+            #is the handle zero?  ... make a new record
+            rec = articles.new()
+    
+    
+    #Is there a form?
+    if request.form:
+        #import pdb; pdb.set_trace()
+        
+        articles.update(rec,request.form)
+        if valid_form(rec):
+            try:
+                articles.save(rec)
+                g.db.commit()
+                return redirect(g.homeURL)
+            except Exception as e:
+                g.db.rollback()
+                flash(printException("Error when attepting to save article.",e))
+                
+    
+    return render_template('news/article_edit.html',rec=rec)
+
+@mod.route('/delete/', methods=['GET','POST'])
+@mod.route('/delete/<int:rec_id>/', methods=['GET','POST'])
+@table_access_required(Article)
+def delete(rec_id=None):
+    setExits()
+    g.title = "Article"
+    
+    if rec_id == None:
+        rec_id = request.form.get('id',request.args.get('id',-1))
+    
+    rec_id = cleanRecordID(rec_id)
+    if rec_id <=0:
+        flash("That is not a valid record ID")
+        return redirect(g.listURL)
+        
+    rec = Article(g.db).get(rec_id)
+    if not rec:
+        flash("Record not found")
+    else:
+        Article(g.db).delete(rec.id)
+        g.db.commit()
+        
+    return redirect(g.listURL)
+
+
+def valid_form(rec):
+    valid_form = True
+    slug = request.form.get('slug','').strip()
+    title = request.form.get('title','').strip()
+    
+    if not slug and title:
+        slug = title.lower()
+        for s in ' /<>"\'#.()':
+            slug = slug.replace(s,'-')
+        rec.slug = slug
+    
+    if not title:
+        flash("The title may not be empty")
+        valid_form = False
+        
+    if not slug:
+        flash("The slug line may not be empty")
+        valid_form = False
+        
+    if slug:
+        sql = 'select * from article where slug = ? and id <> ?'
+        find_slug = Article(g.db).select_raw(sql,(slug,rec.id))
+        if find_slug and len(find_slug) > 0:
+            flash("The slug line must be unique")
+        
+    
+    return valid_form
     
     
